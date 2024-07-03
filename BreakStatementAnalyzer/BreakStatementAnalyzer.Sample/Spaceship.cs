4b35945a02bd5d4f1ddd21cdba8028410f44f4a9
eetrukhan <ekaterina.trukhan@jetbrains.com>
2024-07-03 12:43:05 +0400

    Initial commit

---
+++ BreakStatementAnalyzer/BreakStatementAnalyzer.Sample/Spaceship.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BreakStatementAnalyzer.Sample;
+
+public class Spaceship
+{
+    public void SetSpeed(long speed)
+    {
+        if (speed > 299_792_458)
+            throw new ArgumentOutOfRangeException(nameof(speed));
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i = 0;
+while (true)
+{
+    if (i == 10)
+    {
+        break;      // Non-Compliant
+    }
+
+    Console.WriteLine(i);
+    i++;
+}
+
+Console.WriteLine("Hello, World!");
